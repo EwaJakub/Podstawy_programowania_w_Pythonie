9a2aeec3f9252345f0d7894890c11d9fb8dc2bab
EwaJakub <ewa.jak@onet.pl>
2022-11-03 18:29:26 +0100

    01_Typy_danych

---
+++ 02_Typy_danych_w_Pythonie/01_Typy_danych/task.py
@@ -0,0 +1,9 @@
+answer_1 = 5
+answer_2 = 8.65
+answer_3 = "cat"
+answer_4 = True
+
+print(f"Zmienna answer_1 ma wartość {answer_1}")
+print(f"Zmienna answer_2 ma wartość {answer_2}")
+print(f"Zmienna answer_3 ma wartość {answer_3}")
+print(f"Zmienna answer_4 ma wartość {answer_4}")
