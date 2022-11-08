eceaa37a83fb4be4c10a7b33e5125d58daee30fe
EwaJakub <ewa.jak@onet.pl>
2022-11-08 21:28:34 +0100

    09_Tabliczka_mnożenia_correct

--- 04_Kontrola_przeplywu_programu/09_Tabliczka_mnozenia/task.py
+++ 04_Kontrola_przeplywu_programu/09_Tabliczka_mnozenia/task.py
@@ -1,2 +1,6 @@
-for element in range(1, 9):
-    print(f'liczba: {element}')
+n = 0
+while n < 1 or n > 10:
+    n = int(input('Podaj n od 1 do 10: '))
+
+for element in range(1, 11):
+    print(f'{element} * {n} = {element * n}')
